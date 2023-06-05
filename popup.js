23f53e77615c28f09a65e67994443084f2327ba5
reireirei1221 <reireirei1221@icloud.com>
2023-06-05 22:26:44 +0900

    rating機能が完成しました

--- popup.js
+++ popup.js
@@ -1,111 +1,137 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var questionText = document.getElementById('questionText');
+    var ratingInputs = document.querySelectorAll('#rating input[type="radio"]');
     var sendButton = document.getElementById('sendButton');
+    var ratingSection = document.getElementById('rating');
+    var messageContainer = document.getElementById('messageContainer');
+    var successMessage = document.getElementById('successMessage');
+    var errorMessage = document.getElementById('errorMessage');
+
+    // Add click event listeners to the radio buttons
+    ratingInputs.forEach(function (input) {
+        input.addEventListener('click', function () {
+            // Remove 'active' class from all labels
+            ratingInputs.forEach(function (input) {
+                input.parentNode.classList.remove('active');
+            });
+            // Add 'active' class to the selected label
+            this.parentNode.classList.add('active');
+        });
+    });
+
+    // Add click event listener to the send button
     sendButton.addEventListener('click', function () {
         var selectedRating = document.querySelector('input[name="rating"]:checked');
         if (selectedRating) {
             console.log('Selected rating:', selectedRating.value);
-            sendData(selectedRating.value);
+
+            sendData(selectedRating.value)
+                .then(function (flag_sent) {
+                    console.log(flag_sent);
+
+                    if (flag_sent) {
+                        // Hide rating section and show success message
+                        questionText.style.display = 'none';
+                        ratingSection.style.display = 'none';
+                        sendButton.style.display = 'none';
+                        messageContainer.style.display = 'block';
+                        errorMessage.style.display = 'none';
+                    } else {
+                        // Show error message if no rating is selected
+                        errorMessage.style.display = 'block';
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    // Show error message if an error occurs during data sending
+                    errorMessage.style.display = 'block';
+                });
         } else {
-            console.log('Please select a rating.');
+            // Show error message if no rating is selected
+            errorMessage.style.display = 'block';
         }
     });
 });
 
 function sendData(rating) {
     console.log('Sending data...');
-    getCurrentTab().then((tab) => {
-        var xhr = new XMLHttpRequest();
-        var url = tab.url;
+    return getCurrentTab()
+        .then(function (tab) {
+            return new Promise(function (resolve, reject) {
+                var xhr = new XMLHttpRequest();
 
-        console.log(url);
+                console.log(tab);
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                // リクエストが成功した場合の処理
+                var url = tab.url;
 
-                var parser = new DOMParser();
-                var doc = parser.parseFromString(xhr.responseText, 'text/html');
-                
-                var title = doc.querySelector('.citation__title').innerText;
-                var author_data = doc.querySelectorAll('.loa__author-name');
-                var authors = [];
+                console.log(url);
 
-                for (var i = 0; i < author_data.length; i++) {
-                    var author = author_data[i].innerText;
-                    authors.push(author);
-                }
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        // Request succeeded
+                        var parser = new DOMParser();
+                        var doc = parser.parseFromString(xhr.responseText, 'text/html');
 
-                console.log(title);
-                console.log(authors);
-                console.log(rating);
+                        var title = doc.querySelector('.citation__title').innerText;
+                        var author_data = doc.querySelectorAll('.loa__author-name');
+                        var authors = [];
 
-                var saveXhr = new XMLHttpRequest();
-                var saveUrl = 'https://wordbubbles.herokuapp.com/authors/store-from-outside'; // 実際のデータ送信先URLに置き換えてください
+                        for (var i = 0; i < author_data.length; i++) {
+                            var author = author_data[i].innerText;
+                            authors.push(author);
+                        }
 
-                saveXhr.onreadystatechange = function () {
-                    if (saveXhr.readyState === 4 && saveXhr.status === 200) {
-                        // リクエストが成功した場合の処理
-                        console.log("author stored");
+                        console.log(title);
+                        console.log(authors);
+                        console.log(rating);
+
+                        var saveXhr = new XMLHttpRequest();
+                        var saveUrl = 'https://wordbubbles.herokuapp.com/authors/store-from-outside'; // Replace with the actual data endpoint URL
+
+                        saveXhr.onreadystatechange = function () {
+                            if (saveXhr.readyState === 4 && saveXhr.status === 200) {
+                                // Data stored successfully
+                                console.log('author stored');
+                                resolve(true);
+                            }
+                        };
+
+                        var data = {
+                            title: title,
+                            authors: authors,
+                            rating: rating
+                        };
+
+                        console.log(data);
+
+                        saveXhr.open(
+                            'GET',
+                            saveUrl + '?data=' + encodeURIComponent(JSON.stringify(data)),
+                            true
+                        );
+                        saveXhr.send(null);
                     }
                 };
 
-                var data = {
-                    title: title,
-                    authors: authors,
-                    rating: rating
-                };
+                xhr.open('GET', url, true);
+                xhr.send();
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
+            return false;
+        });
+}
 
-                console.log(data);
-
-                saveXhr.open('GET', saveUrl + '?data=' + encodeURIComponent(JSON.stringify(data)), false);
-                saveXhr.send(null);
+function getCurrentTab() {
+    return new Promise(function (resolve, reject) {
+        let queryOptions = { active: true, lastFocusedWindow: true };
+        chrome.tabs.query(queryOptions, function (tabs) {
+            if (tabs && tabs.length > 0) {
+                resolve(tabs[0]);
+            } else {
+                reject('Unable to get the current tab');
             }
-        };
-
-        xhr.open('GET', url, true);
-        xhr.send();
-        // chrome.tabs.sendMessage(tab.id, { rating: rating });
-        // console.log('Data sent!');
-
+        });
     });
 }
-
-async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-}
-
-function save_paper() {
-    console.log("save_paper called");
-}
-
-// function save_paper(rating) {
-//     var title = document.getElementsByClassName("citation__title")[0].innerText;
-//     var author_data = document.getElementsByClassName("loa__author-name");
-//     var authors = [];
-//     for (var i = 0; i < author_data.length; i++) {
-//         authors.push(author_data[i].innerText);
-//     }
-
-//     var xhr = new XMLHttpRequest();
-//     var url = 'https://wordbubbles.herokuapp.com/authors/store-from-outside'; // 実際のデータ送信先URLに置き換えてください
-
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === 4 && xhr.status === 200) {
-//             // リクエストが成功した場合の処理
-//             console.log("author stored");
-//         }
-//     };
-
-//     var data = {
-//         title: title,
-//         authors: authors,
-//         rating: rating
-//     };
-
-//     xhr.open('GET', url + '?data=' + encodeURIComponent(JSON.stringify(data)), true);
-//     xhr.setRequestHeader('Content-Type', 'application/json');
-//     xhr.send();
-// }
